229399099a6b79e79388ff5c9354d20a307ec31f
MauryDev <41791247+MauryDev@users.noreply.github.com>
2019-10-03 19:28:43 -0300

    Create tester.js

---
+++ tester.js
@@ -0,0 +1,4 @@
+setInterval(() => {
+for (let i = 0; i < document.getElementsByClassName("_40qZj PTP5j").length; i++) {
+    document.getElementsByClassName("_40qZj PTP5j")[i].innerHTML = "Em jogo seção"
+}})
