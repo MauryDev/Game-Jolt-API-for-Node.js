d2a8220f53ea4c75d5cd897f98ec7166dcb62358
MauryDev <41791247+MauryDev@users.noreply.github.com>
2019-10-03 19:31:43 -0300

    Update tester.js

--- tester.js
+++ tester.js
@@ -1,4 +1,5 @@
 setInterval(() => {
-for (let i = 0; i < document.getElementsByClassName("_40qZj PTP5j").length; i++) {
-    document.getElementsByClassName("_40qZj PTP5j")[i].innerHTML = "Em jogo seção"
-}})
+    for (let i = 0; i < document.getElementsByClassName("_40qZj PTP5j").length; i++) {
+        document.getElementsByClassName("_40qZj PTP5j")[i].innerHTML = "Em jogo seção";
+    }
+},1000);
