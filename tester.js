55c7ee8f51f90b62d253d8e835220fe930a36c3b
MauryDev <maurygta2@gmail.com>
2019-12-13 03:31:43 +0000

    tester

--- tester.js
+++
@@ -1,9 +0,0 @@
-setInterval(() => {
-    for (let i = 0; i < document.getElementsByClassName("_40qZj PTP5j").length; i++) {
-        var _40qZj_PTP5j = document.getElementsByClassName("_40qZj PTP5j")[i];
-        
-        if (_40qZj_PTP5j.innerHTML == "In game session") {
-            document.getElementsByClassName("_40qZj PTP5j")[i].innerHTML = "No jogo, entrar ?";
-        }
-    }
-},1000);
